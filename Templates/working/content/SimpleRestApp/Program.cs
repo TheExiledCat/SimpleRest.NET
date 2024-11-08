f15fd19cdf0c9e1f1d9eb59667672964a7c6a7d7
TheExiledCat <kazekage2002@hotmail.com>
2024-11-08 10:11:22 +0100

    added templating structure

---
+++ Templates/working/content/SimpleRestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleRestApp;
+
+using SimpleRest.Api;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        SimpleRestApi api = new SimpleRestApi(3000, new SimpleRestLogger(SimpleRestLogLevel.DEBUG));
+        api.Get(
+            "/",
+            async (req, res) =>
+            {
+                res.Send("Hello World");
+            }
+        );
+        await api.Start(
+            (port, url) =>
+            {
+                Console.WriteLine("Started server: " + url);
+            }
+        );
+    }
+}
